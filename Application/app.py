205d88cf396440126fd85cb801aed1dc5dde449a
Dakshata Chiman <dakshatachiman81@gmail.com>
2026-09-10 18:28:31 +0530

    Update application UI to v3

--- Application/app.py
+++ Application/app.py
@@ -12,7 +12,7 @@ def home():
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>DevOps Cloud Platform</title>
+    <title>DevOps Cloud Platform - v3</title>
 
     <style>
         * {
